19071109f79fdf9f93bbfca5562747cb3df5e342
Ross_Medvid <54904839+RossMedvid@users.noreply.github.com>
2019-09-16 20:59:24 +0300

    Add files via upload

---
+++ HW_2_Task_2.py
@@ -0,0 +1,5 @@
+n=input("Please type 4-digit number: ")
+print("Добуток:",(int(n[0]))*(int(n[1]))*(int(n[2])*(int(n[3]))))
+print("Реверсний порядок:", n[3]+n[2]+n[1]+n[0])
+list_of_numbers=[int(n[0]),int(n[1]),int(n[2]),int(n[3])]
+print("Посортовані числа:",sorted(list_of_numbers))
